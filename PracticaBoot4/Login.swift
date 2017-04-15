46bdb720634f458388bfc65976d4b1b15cd12193
Eugenio Barquin <eu.barquin@me.com>
2017-04-15 12:45:07 +0200

    Modifications in StoryBoard to implement login view controller. Also outlets done

---
+++ PracticaBoot4/Login.swift
@@ -0,0 +1,28 @@
+//
+//  Login.swift
+//  PracticaBoot4
+//
+//  Created by Eugenio Barquín on 15/4/17.
+//  Copyright © 2017 COM. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class Login: UIViewController {
+    
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBAction func loginRegisterAction(_ sender: Any) {
+    }
+    
+    
+
+
+
