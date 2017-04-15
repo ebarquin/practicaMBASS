871e2f97077cd9b655c7f626b1af539050d5ab22
Eugenio Barquin <eu.barquin@me.com>
2017-04-15 14:06:08 +0200

    Login UI  and Register implementation done

--- PracticaBoot4/Login.swift
+++ PracticaBoot4/Login.swift
@@ -18,11 +18,45 @@ class Login: UIViewController {
     
     @IBOutlet weak var passwordTextField: UITextField!
     
+    @IBOutlet weak var inputsViewHeightConstraint: NSLayoutConstraint!
+
+    @IBOutlet weak var loginRegisterSegmentedControl: UISegmentedControl!
     
-    @IBAction func loginRegisterAction(_ sender: Any) {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loginRegisterSegmentedControl.selectedSegmentIndex = 1
     }
     
+    @IBAction func handleLoginRegisterChange(_ sender: Any) {
+        if loginRegisterSegmentedControl.selectedSegmentIndex == 0 {
+            nameTextField.isHidden = true
+            inputsViewHeightConstraint.constant = 100
+            
+        } else if loginRegisterSegmentedControl.selectedSegmentIndex == 1 {
+            nameTextField.isHidden = false
+            inputsViewHeightConstraint.constant = 150
+        }
+        
+    }
+    @IBAction func loginRegisterAction(_ sender: Any) {
+        handleRegister()
+    }
     
+    func handleRegister() {
+        guard let name = nameTextField.text, let email = emailTextField.text, let password = passwordTextField.text else {
+            print("Form not valid")
+            return
+        }
+        FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
+            if error != nil {
+                print(error)
+                return
+            }
+        })
+        
+        
+    }
 }
 
 
