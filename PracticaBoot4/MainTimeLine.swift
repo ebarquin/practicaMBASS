101bed3715b418dcf3f959661ceff5190fc25d2a
Eugenio Barquin <eu.barquin@me.com>
2017-04-20 20:22:57 +0200

    Correction in TableView to avoid duplicates, implementing unsubscribe to the FB observer

--- PracticaBoot4/MainTimeLine.swift
+++ PracticaBoot4/MainTimeLine.swift
@@ -10,28 +10,35 @@ import UIKit
 import Firebase
 
 class MainTimeLine: UITableViewController {
-
+    
     var model : [Post] = []
     let cellIdentifier = "POSTSCELL"
+    var postsRef: FIRDatabaseReference!
+    var handle: UInt = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        FIRAnalytics.setScreenName("MainTimeLine", screenClass: "Main")
+        
+        self.refreshControl?.addTarget(self, action: #selector(hadleRefresh(_:)), for: UIControlEvents.valueChanged)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
         var postsDic: [Post] = []
         getAllPublishedPostsFromFB { (posts) in
             postsDic = posts
             self.model = postsDic
             self.tableView.reloadData()
         }
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-        FIRAnalytics.setScreenName("MainTimeLine", screenClass: "Main")
         
-        self.refreshControl?.addTarget(self, action: #selector(hadleRefresh(_:)), for: UIControlEvents.valueChanged)
     }
+    override func viewWillDisappear(_ animated: Bool) {
+        postsRef.removeObserver(withHandle: handle)
+    }
+    
     
     @IBAction func handleLogout(_ sender: Any) {
         
@@ -46,7 +53,7 @@ class MainTimeLine: UITableViewController {
         }
         
     }
-
+    
     func hadleRefresh(_ refreshControl: UIRefreshControl) {
         refreshControl.endRefreshing()
     }
@@ -54,38 +61,38 @@ class MainTimeLine: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return model.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell.init(style: .subtitle, reuseIdentifier: cellIdentifier)
-
+        
         cell.imageView?.image = UIImage(imageLiteralResourceName: "dummyProfilePicture")
         cell.textLabel?.text = model[indexPath.row].title
         cell.detailTextLabel?.text = model[indexPath.row].postText
         
-
+        
         return cell
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         performSegue(withIdentifier: "ShowRatingPost", sender: indexPath)
     }
-
-
+    
+    
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
@@ -95,15 +102,15 @@ class MainTimeLine: UITableViewController {
             // aqui pasamos el item selecionado
         }
     }
-
+    
     func getAllPublishedPostsFromFB(completion:@escaping ([Post]) -> Void) {
         
         var posts: [Post] = []
         
-        let postsRef = FIRDatabase.database().reference().child("posts")
+        postsRef = FIRDatabase.database().reference().child("posts")
         let query = postsRef.queryOrdered(byChild: "published").queryEqual(toValue: true)
         
-        query.observe(FIRDataEventType.value, with: { ( snap ) in
+        handle = query.observe(FIRDataEventType.value, with: { ( snap ) in
             
             for postFB in snap.children {
                 
@@ -121,8 +128,8 @@ class MainTimeLine: UITableViewController {
         
     }
     
-
     
     
-
+    
+    
 }
